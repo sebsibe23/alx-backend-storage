885035196edf162e8a2ee1ffcff5f40140c1e89e
sebsibe23 <sebsibe1990@proton.me>
2024-05-09 02:18:22 -0700

    q3 final fix

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,8 +3,5 @@
 -- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE()))
 -- - formed) AS lifespan
 -- @author sebsibe solomon https://github.com/sebsibe23
-SELECT band_name,
-       IF(split IS NULL, 2022 - formed, split - formed) AS lifespan
-FROM metal_bands
-WHERE main_style = 'Glam rock'
-ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, 2022) - formed)
+AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
