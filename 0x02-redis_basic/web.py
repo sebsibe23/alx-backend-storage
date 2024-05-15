ff2ba574f24c43f0d5d8ed09dd7bd72be21f26de
sebsibe23 <sebsibe1990@proton.me>
2024-05-15 06:57:57 -0700

    web fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,45 +1,34 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+""" expiring web cache module """
+
 import redis
 import requests
-from functools import wraps
 from typing import Callable
+from functools import wraps
+
+redis = redis.Redis()
 
 
-redis_store = redis.Redis()
-'''a module-level Redis instance.
-'''
+def wrap_requests(fn: Callable) -> Callable:
+    """ Decorator wrapper """
+
+    @wraps(fn)
+    def wrapper(url):
+        """ Wrapper for decorator guy """
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
+        return result
+
+    return wrapper
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches a output of fetched data.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''a wrapper function for caching the output.
-        '''
-        try:
-            redis_store.incr(f'count:{url}')
-            result = redis_store.get(f'result:{url}')
-            if result:
-                return result.decode('utf-8')
-            result = method(url)
-            redis_store.set(f'count:{url}', 0)
-            redis_store.setex(f'result:{url}', 10, result)
-            return result
-        except Exception as e:
-            print(e)
-
-    return invoker
-
-
-@data_cacher
+@wrap_requests
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    try:
-        return requests.get(url).text
-    except Exception as e:
-        print(e)
+    """get page self descriptive
+    """
+    response = requests.get(url)
+    return response.text
