5b0c51409c55919e16938b0bc1a7f83e805bb276
sebsibe23 <sebsibe1990@proton.me>
2024-05-15 09:07:54 -0700

    last web task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,24 +1,49 @@
 #!/usr/bin/env python3
+'''
+A module with tools for request caching and tracking.
+'''
+import redis
 import requests
-from functools import lru_cache
-
-# Create a dictionary to store the count of URL accesses
-url_count = {}
+from functools import wraps
+from typing import Callable
 
 
-@lru_cache(maxsize=32)
+redis_store = redis.Redis()
+'''
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """The module-level Redis instance.
+'''
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''
+    Returns:
+        [type]: [description]
+      Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''
+         a wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
 def get_page(url: str) -> str:
-    # Increase the count of the URL
-    url_count[url] = url_count.get(url, 0) + 1
-
-    # Get the HTML content of the URL
-    response = requests.get(url)
-
-    # Return the HTML content
-    return response.text
-
-
-# Test the function with a slow URL
-url = "http://slowwly.robertomurray.co.uk"
-print(get_page(url))
-print("URL count:", url_count[url])
+    '''Returns the content of a URL after caching
+    the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
