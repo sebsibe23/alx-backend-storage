dfc1272b6fd80129fc5bd1519c890854c04ad847
sebsibe23 <sebsibe1990@proton.me>
2024-05-15 09:03:14 -0700

    final push

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,24 @@
 #!/usr/bin/env python3
-from datetime import datetime, timedelta
-from requests import get
+import requests
+from functools import lru_cache
 
-cache = {}
-cache_expiration = 10  # Seconds
-
-
-def access_counter(url):
-    cache_key = f"count:{url}"
-    if cache_key not in cache:
-        cache[cache_key] = 0
-    cache[cache_key] += 1
+# Create a dictionary to store the count of URL accesses
+url_count = {}
 
 
-def cache_decorator(func):
-    def wrapper(url):
-        cache_key = f"{func.__name__}:{url}"
-        current_time = datetime.now()
-        if (
-            cache_key in cache
-            and (current_time - cache[cache_key]["time"]).total_seconds()
-            < cache_expiration
-        ):
-            return cache[cache_key]["data"]
-        else:
-            data = func(url)
-            cache[cache_key] = {"data": data, "time": current_time}
-            access_counter(url)
-            return data
+@lru_cache(maxsize=32)
+def get_page(url: str) -> str:
+    # Increase the count of the URL
+    url_count[url] = url_count.get(url, 0) + 1
 
-    return wrapper
+    # Get the HTML content of the URL
+    response = requests.get(url)
 
-
-@cache_decorator
-def get_page(url):
-    response = get(url)
+    # Return the HTML content
     return response.text
+
+
+# Test the function with a slow URL
+url = "http://slowwly.robertomurray.co.uk"
+print(get_page(url))
+print("URL count:", url_count[url])
